1e668e8ce582e76ac2da757f2a1e8b7911b5fa94
diatlova <dari.diatlova@gmail.com>
2022-01-15 19:07:18 +0300

    added scrip for clean files moves

---
+++ clean_files_train_test.sh
@@ -0,0 +1,30 @@
+source_directory="/home/dadyatlova_1/russian_speech_denoiser/DNS-Challenge/datasets/training_set_jan14_min35_10_40h"
+
+TRAIN_DIR="$source_directory/noisy_train"
+VAL_DIR="$source_directory/noisy_val"
+TEST_DIR="$source_directory/noisy_test"
+
+declare -a TRAIN_ARRAY=($(ls $TRAIN_DIR))
+declare -a VAL_ARRAY=($(ls $VAL_DIR))
+declare -a TEST_ARRAY=($(ls $TEST_DIR))
+
+mkdir "$source_directory/clean_train"
+mkdir "$source_directory/clean_val"
+mkdir "$source_directory/clean_test"
+
+for name in ${TRAIN_ARRAY[@]}; do
+  i=${name##*_}
+  cp "${source_directory}/clean/clean_fileid_${i}" "$source_directory/clean_train"
+done
+
+for name in ${VAL_ARRAY[@]}; do
+  i=${name##*_}
+  cp "${source_directory}/clean/clean_fileid_${i}" "$source_directory/clean_val"
+done
+
+for name in ${TRAIN_ARRAY[@]}; do
+  i=${name##*_}
+  cp "${source_directory}/clean/clean_fileid_${i}" "$source_directory/clean_test"
+done
+
+echo "Files were copied successfully"
